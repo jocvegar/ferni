42c5ed73d32f2a461657366775c16879fa6857ed
Jose Vega <jocvegar@gmail.com>
2022-02-04 09:46:02 -0600

    test asyncData

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app dark>
     <v-navigation-drawer
-      v-model="drawer"
+      v-if="this.$vuetify.breakpoint.mdAndUp"
       :mini-variant="miniVariant"
       :clipped="clipped"
       fixed
@@ -24,9 +24,34 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
+    <v-navigation-drawer v-else v-model="leftDrawer" temporary fixed>
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
     <v-app-bar :clipped-left="clipped" fixed app>
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
+      <v-btn
+        icon
+        @click.stop="miniVariant = !miniVariant"
+        v-if="this.$vuetify.breakpoint.mdAndUp"
+      >
         <v-icon>mdi-{{ `chevron-${miniVariant ? "right" : "left"}` }}</v-icon>
+      </v-btn>
+      <v-btn icon @click.stop="leftDrawer = !leftDrawer" v-else>
+        <v-icon>mdi-{{ `chevron-${leftDrawer ? "right" : "left"}` }}</v-icon>
       </v-btn>
       <v-toolbar-title v-text="title" />
       <v-spacer />
@@ -39,7 +64,7 @@
         <Nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
+    <v-navigation-drawer v-model="rightDrawer" right temporary fixed>
       <v-list>
         <v-list-item>
           <v-list-item-action>
@@ -49,7 +74,7 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-footer :absolute="!fixed" app>
+    <v-footer absolute app>
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
   </v-app>
@@ -61,8 +86,6 @@ export default {
   data() {
     return {
       clipped: false,
-      drawer: false,
-      fixed: false,
       items: [
         {
           icon: "mdi-account-check",
@@ -76,9 +99,9 @@ export default {
         },
       ],
       miniVariant: true,
-      right: true,
       rightDrawer: false,
-      title: "Vuetify.js",
+      leftDrawer: false,
+      title: "Dr. Alvarenga",
     };
   },
 };
