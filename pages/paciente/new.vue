e701f783142dccc62ed2682e435ac9d7899aa55a
Jose Vega <jocvegar@gmail.com>
2022-02-04 08:13:42 -0600

    add pages

---
+++ pages/paciente/new.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-card elevation="0">
+    <v-card-actions class="float-right">
+      <v-btn color="primary" elevation="2" raised to="/" nuxt outlined>
+        <v-icon>mdi-chevron-left</v-icon>
+        Atras
+      </v-btn>
+    </v-card-actions>
+    <v-card-title><h2>Nuevo Paciente</h2></v-card-title>
+  </v-card>
+</template>
+<script>
+export default {
+  name: "PacienteNew",
+};
+</script>
