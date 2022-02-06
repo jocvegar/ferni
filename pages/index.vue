20e2a260a7e7b8f582ca1f162e189ea9f7728543
Jose Vega <jocvegar@gmail.com>
2022-02-05 21:53:40 -0600

    fix title

--- pages/index.vue
+++ pages/index.vue
@@ -17,124 +17,76 @@
         hide-details
       ></v-text-field>
     </v-card-title>
-    <v-data-table
-      :headers="headers"
-      :items="users"
-      :search="search"
-    ></v-data-table>
+    <v-data-table :headers="headers" :items="pacientes" :search="search">
+      <template v-slot:[`item.fecha_de_nacimiento`]="{ item }">
+        {{ getAge(item.fecha_de_nacimiento) }}
+      </template></v-data-table
+    >
   </v-card>
 </template>
 
 <script>
 import auth from "@/mixins/authMixin";
+import { db } from "~/plugins/firebase.js";
+import { collection, onSnapshot } from "firebase/firestore";
 
 export default {
   name: "Index",
   mixins: [auth],
-  async asyncData({ $axios }) {
-    const users = await $axios.$get(
-      "https://jsonplaceholder.typicode.com/users"
-    );
-
-    // const desserts = [
-    //   {
-    //     name: "Frozen Yogurt",
-    //     calories: 159,
-    //     fat: 6.0,
-    //     carbs: 24,
-    //     protein: 4.0,
-    //     iron: "1%",
-    //   },
-    //   {
-    //     name: "Ice cream sandwich",
-    //     calories: 237,
-    //     fat: 9.0,
-    //     carbs: 37,
-    //     protein: 4.3,
-    //     iron: "1%",
-    //   },
-    //   {
-    //     name: "Eclair",
-    //     calories: 262,
-    //     fat: 16.0,
-    //     carbs: 23,
-    //     protein: 6.0,
-    //     iron: "7%",
-    //   },
-    //   {
-    //     name: "Cupcake",
-    //     calories: 305,
-    //     fat: 3.7,
-    //     carbs: 67,
-    //     protein: 4.3,
-    //     iron: "8%",
-    //   },
-    //   {
-    //     name: "Gingerbread",
-    //     calories: 356,
-    //     fat: 16.0,
-    //     carbs: 49,
-    //     protein: 3.9,
-    //     iron: "16%",
-    //   },
-    //   {
-    //     name: "Jelly bean",
-    //     calories: 375,
-    //     fat: 0.0,
-    //     carbs: 94,
-    //     protein: 0.0,
-    //     iron: "0%",
-    //   },
-    //   {
-    //     name: "Lollipop",
-    //     calories: 392,
-    //     fat: 0.2,
-    //     carbs: 98,
-    //     protein: 0,
-    //     iron: "2%",
-    //   },
-    //   {
-    //     name: "Honeycomb",
-    //     calories: 408,
-    //     fat: 3.2,
-    //     carbs: 87,
-    //     protein: 6.5,
-    //     iron: "45%",
-    //   },
-    //   {
-    //     name: "Donut",
-    //     calories: 452,
-    //     fat: 25.0,
-    //     carbs: 51,
-    //     protein: 4.9,
-    //     iron: "22%",
-    //   },
-    //   {
-    //     name: "KitKat",
-    //     calories: 518,
-    //     fat: 26.0,
-    //     carbs: 65,
-    //     protein: 7,
-    //     iron: "6%",
-    //   },
-    // ];
-    return { users };
+  head: {
+    title: "Pacientes",
+  },
+  async asyncData() {
+    const pacientesDocRef = collection(db, "pacientes");
+    let pacientes = [];
+    return new Promise((resolve, reject) => {
+      onSnapshot(
+        pacientesDocRef,
+        { includeMetadataChanges: true },
+        (snapshot) => {
+          snapshot.forEach((doc) => {
+            pacientes.push({
+              id: doc.id,
+              ...doc.data(),
+            });
+          });
+          resolve({ pacientes });
+        }
+      );
+    });
   },
   data() {
     return {
       search: "",
       headers: [
         {
-          text: "User Id",
+          text: "Nombre",
           align: "start",
-          value: "id",
+          value: "nombre",
         },
-        { text: "Name", value: "name" },
-        { text: "Email", value: "email" },
-        { text: "Phone", value: "phone" },
-        { text: "Website", value: "website" },
+        { text: "Edad", value: "fecha_de_nacimiento" },
+        { text: "Antecedentes", value: "antecedentes" },
+        { text: "Dedicación", value: "a_que_se_dedica" },
+        { text: "Pasatiempos", value: "pasatiempos" },
+        { text: "Procedencia", value: "procedencia" },
       ],
     };
+  },
+  methods: {
+    getAge(dob) {
+      if (dob) {
+        const today = new Date();
+        const birthDate = new Date(dob?.seconds * 1000);
+        let age = today.getFullYear() - birthDate.getFullYear();
+        const m = today.getMonth() - birthDate.getMonth();
+        if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+          age--;
+        }
+        return age;
+      } else {
+        return "sin información";
+      }
+    },
   },
 };
 </script>
